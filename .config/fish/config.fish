6c5ff92b3e47f84ce4f5b633abe9199b2fe7c770
Cornfy <lzentye@gmail.com>
2026-09-05 17:23:46 +0800

    Chore: added paru config, and updete fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,14 +6,20 @@ if status is-interactive
 
     # Setup common aliases
     alias ls='eza -la --color=auto --group-directories-first --group'
-    alias cat='bat'
     alias lsblk='lsblk -o NAME,LABEL,TYPE,SIZE,MOUNTPOINTS,UUID'
     alias grep='grep --color=auto'
-    alias cleanup='yay -Rsnc $(yay -Qqdt)'
     alias vim='nvim'
+    alias aur='paru'
+    alias winer='bwrap-winer'
+    alias cleanup='paru -Rsnc (paru -Qdtq)'
     alias ssh='env TERM=xterm-256color ssh'
     alias f='fastfetch'
     alias c='clear'
+
+    # Key bindinds
+    bind \cf ff-insert
+    bind \cy y
+    bind \cg lazygit
 end
 
 # Add user bin directories to PATH
@@ -23,11 +29,8 @@ fish_add_path "$HOME/bin"
 # GO Proxy Setting
 set -gx GOPROXY "https://goproxy.cn"
 
-# Use ff scrept (~/.local/bin/ff) to return path
-bind \cf ff-insert
-
-# yazi here
-bind \cy y
-
-# lazygit here
-bind \cg lazygit
+# Android SDK
+set -gx ANDROID_HOME "$HOME/Android/Sdk"
+set -gx ANDROID_SDK_ROOT "$HOME/Android/Sdk"
+fish_add_path -g "$ANDROID_HOME/cmdline-tools/latest/bin"
+fish_add_path -g "$ANDROID_HOME/platform-tools"
